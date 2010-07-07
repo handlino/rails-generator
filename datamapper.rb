cc5c3f6b4007179b51589df8efa3fffca10ce66f
Kang-min Liu <gugod@gugod.org>
2010-07-07 18:35:19 +0800

    add a template for datamapper

---
+++ datamapper.rb
@@ -0,0 +1,132 @@
+gem "will_paginate", "3.0.pre"
+
+apply 'http://datamapper.org/templates/rails/gemfile.rb'
+apply 'http://datamapper.org/templates/rails/config.rb'
+apply 'http://datamapper.org/templates/rails/database.yml.rb'
+
+inject_into_file  'app/controllers/application_controller.rb',
+                  "require 'dm-rails/middleware/identity_map'\n",
+                  :before => 'class ApplicationController'
+
+inject_into_class 'app/controllers/application_controller.rb',
+                  'ApplicationController',
+                  "  use Rails::DataMapper::Middleware::IdentityMap\n"
+
+# remove unused files
+run "rm README"
+run "rm public/index.html"
+run "rm -f public/javascripts/*"
+run "rm -f publuc/images/*"
+
+# create root path
+
+generate :controller, "Welcome index"
+route "map.root :controller => 'welcome'"
+
+# install unit test framework
+
+gem "factory_girl_rails",   ">= 1.0.0",          :group => [:test, :cucumber]
+gem "rspec-rails",          ">= 2.0.0.beta.9.1",  :group => [:test, :cucumber]
+
+generators = <<-GENERATORS
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+GENERATORS
+
+application generators
+
+# handlino gems
+
+gem 'handicraft_helper',  ">=1.1"
+
+# gems
+
+gem 'will_paginate',  ">=3.0.pre"
+gem 'delayed_job'
+gem 'paperclip'
+gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => 'rails3'
+ 
+
+# JQuery
+
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
+get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+
+
+if yes?("Using Handicraft-theme ?")
+  if File.directory?("../handicraft-theme")
+    run "rm app/views/layouts/application.html.erb"
+    run "cp ../handicraft-theme/app/views/layouts/fluid.html.erb app/views/layouts/application.html.erb"
+
+    run "mkdir app/views/common"
+    run "cp ../handicraft-theme/app/views/common/_main_navigation.html.erb app/views/common/_main_navigation.html.erb"
+    run "cp ../handicraft-theme/app/views/common/_user_navigation.html.erb app/views/common/_user_navigation.html.erb"
+   
+    run "cp ../handicraft-theme/public/stylesheets/stickie.css public/stylesheets/"
+    run "mkdir public/stylesheets/handicraft/"
+    run "cp -R ../handicraft-theme/public/stylesheets/handicraft/* public/stylesheets/handicraft/"
+
+    run "cp -R ../handicraft-theme/public/javascripts/core.js public/javascripts/"
+  else
+  end
+end
+
+jquery = <<-JQUERY
+module ActionView::Helpers::AssetTagHelper
+  remove_const :JAVASCRIPT_DEFAULT_SOURCES
+  JAVASCRIPT_DEFAULT_SOURCES = %w(jquery.js rails.js core.js application.js)
+
+  reset_javascript_include_default
+end
+JQUERY
+
+initializer "jquery.rb", jquery
+
+# for git
+gitignore = <<-GITIGNORE
+Thumbs.db
+.DS_Store
+tmp/
+sphinx_*.*
+*.log
+*.pid
+config/database.yml
+*~
+db/schema.rb
+db/development_structure.sql
+public/javascripts/all.js
+public/stylesheets/all.css
+coverage/
+rcov/
+public/system/*
+config/*.sphinx.conf
+db/sphinx/*
+db/*.sqlite3
+test/*
+*.swp
+GITIGNORE
+
+remove_file ".gitignore"
+create_file ".gitignore", gitignore
+
+git :init
+git :add => "."
+
+run "bundle install"
+run "rails g authlogic:session user_session"
+
+docs = <<-DOCS
+
+Run the following commands to complete the setup of #{app_name.humanize}:
+
+% cd #{app_name}
+% bundle install
+% bundle lock
+% script/rails generate rspec:install
+
+DOCS
+
+log docs
