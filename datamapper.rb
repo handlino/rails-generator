afe0c4939253b0b5616dd9d030f642632b7f9193
Kang-min Liu <gugod@gugod.org>
2010-07-07 20:34:22 +0800

    declare rails gems first to avoid a gem activation problem.

--- datamapper.rb
+++ datamapper.rb
@@ -2,16 +2,15 @@
 run "rm -f Gemfile README public/index.html public/javascripts/* publuc/images/*"
 create_file 'Gemfile'
 
-apply File.dirname(__FILE__) + '/common/gems.rb'
-
 RAILS_VERSION = '~> 3.0.0.beta4'
 gem 'activesupport',      RAILS_VERSION, :require => 'active_support'
 gem 'actionpack',         RAILS_VERSION, :require => 'action_pack'
 gem 'actionmailer',       RAILS_VERSION, :require => 'action_mailer'
 gem 'railties',           RAILS_VERSION, :require => 'rails'
 
+apply File.dirname(__FILE__) + '/common/gems.rb'
+
 gem 'dm-rails'
-gem 'dm-sqlite-adapter'
 gem 'dm-mysql-adapter'
 gem 'dm-migrations'
 gem 'dm-types'
@@ -25,8 +24,48 @@ gem 'dm-core'
 gem 'dm-do-adapter'
 gem 'dm-active_model'
 
-apply 'http://datamapper.org/templates/rails/config.rb'
-apply 'http://datamapper.org/templates/rails/database.yml.rb'
+# http://datamapper.org/templates/rails/config.rb
+gsub_file 'config/application.rb', /require 'rails\/all'/ do
+<<-RUBY
+## Pick the frameworks you want:
+require 'action_controller/railtie'
+require 'dm-rails/railtie'
+# require 'action_mailer/railtie'
+# require 'active_resource/railtie'
+# require 'rails/test_unit/railtie'
+RUBY
+end
+
+gsub_file 'config/environments/development.rb', /config.action_mailer.raise_delivery_errors = false/ do
+  "# config.action_mailer.raise_delivery_errors = false"
+end
+
+gsub_file 'config/environments/test.rb', /config.action_mailer.delivery_method = :test/ do
+  "# config.action_mailer.delivery_method = :test"
+end
+##############################################################
+
+app_name = app_path.split('/').last.downcase
+create_file 'config/database.yml.example' do
+<<-YAML
+defaults: &defaults
+  adapter: mysql
+  username: root
+  password:
+  host: localhost
+
+development:
+  <<: *defaults
+  database: #{app_name}_development
+test:
+  <<: *defaults
+  database: #{app_name}_test
+production:
+  <<: *defaults
+  database: #{app_name}_production
+YAML
+end
+run "cp config/database.yml.example config/database.yml"
 
 inject_into_file  'app/controllers/application_controller.rb',
                   "require 'dm-rails/middleware/identity_map'\n",
@@ -43,12 +82,13 @@ application <<-GENERATORS
     end
 GENERATORS
 
-# run "bundle install"
-run "rails g authlogic:session user_session"
+run "bundle install"
 
 # create root path
 generate :controller, "Welcome index"
 route "map.root :controller => 'welcome'"
+
+generate "authlogic:session", "user_session"
 
 apply File.dirname(__FILE__) + '/common/jquery.rb'
 apply File.dirname(__FILE__) + '/common/handicraft-theme.rb'
@@ -61,6 +101,7 @@ Run the following commands to complete the setup of #{app_name.humanize}:
 % cd #{app_name}
 % bundle install
 % bundle lock
-% script/rails generate rspec:install
+% rails g authlogic:session user_session
+% rails g rspec:install
 
 DOCS
