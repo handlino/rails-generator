5709c14dde1a2ec82d719b29e701431d39c07b21
Kang-min Liu <gugod@gugod.org>
2010-07-07 20:04:53 +0800

    the :group does not map to env, plus it is not required to have a :cucumber group

--- common/gems.rb
+++ common/gems.rb
@@ -7,10 +7,11 @@ gem 'authlogic', :git => 'git://github.com/odorcicd/authlogic.git', :branch => '
 
 RSPEC_VERSION = ">= 2.0.0.beta.9.1"
 
-gem 'rspec',              RSPEC_VERSION, :group => [:test, :cucumber]
-gem 'rspec-core',         RSPEC_VERSION, :group => [:test, :cucumber], :require => 'rspec/core'
-gem 'rspec-expectations', RSPEC_VERSION, :group => [:test, :cucumber], :require => 'rspec/expectations'
-gem 'rspec-mocks',        RSPEC_VERSION, :group => [:test, :cucumber], :require => 'rspec/mocks'
-gem 'rspec-rails',        RSPEC_VERSION, :group => [:test, :cucumber]
-gem "factory_girl_rails", ">= 1.0.0",    :group => [:test, :cucumber]
+gem 'rspec',              RSPEC_VERSION, :group => :test
+gem 'rspec-core',         RSPEC_VERSION, :group => :test, :require => 'rspec/core'
+gem 'rspec-expectations', RSPEC_VERSION, :group => :test, :require => 'rspec/expectations'
+gem 'rspec-mocks',        RSPEC_VERSION, :group => :test, :require => 'rspec/mocks'
+gem 'rspec-rails',        RSPEC_VERSION, :group => :test
+gem "factory_girl",       ">= 1.0.0",    :group => :test
+gem "factory_girl_rails", ">= 1.0.0",    :group => :test
 
