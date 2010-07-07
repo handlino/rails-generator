cea8542d2a4975241843b44eff24d142610d347e
Kang-min Liu <gugod@gugod.org>
2010-07-07 22:34:51 +0800

    the jquery generator included in rails3-generator has done the equivalent.

--- common/jquery.rb
+++ common/jquery.rb
@@ -1,12 +1,1 @@
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery-1.4.2.min.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
-
-
-initializer <<-JQUERY
-module ActionView::Helpers::AssetTagHelper
-  remove_const :JAVASCRIPT_DEFAULT_SOURCES
-  JAVASCRIPT_DEFAULT_SOURCES = %w(jquery-1.4.2.min.js rails.js core.js application.js)
-
-  reset_javascript_include_default
-end
-JQUERY
+generate "jquery:install"
